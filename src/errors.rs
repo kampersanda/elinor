09448839baea9e417f4f91413a1a8a3d7ca7be95
kampersanda <shnsk.knd@gmail.com>
2024-09-14 16:51:51 +0900

    add

--- src/errors.rs
+++ src/errors.rs
@@ -4,15 +4,15 @@ use thiserror::Error;
 /// Error types for Emir.
 #[derive(Error, Debug)]
 pub enum EmirError {
-    /// Error when query-document pairs are duplicated.
+    /// Error when an entry is duplicated.
     #[error("{0}")]
     DuplicateEntry(String),
 
-    /// Error when a query is missing.
+    /// Error when an entry is missing.
     #[error("{0}")]
     MissingEntry(String),
 
-    /// Error when a document is missing.
+    /// Error when the format is invalid.
     #[error("{0}")]
     InvalidFormat(String),
 }
