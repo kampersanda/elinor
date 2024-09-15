31b2b3129d1a1838cce2cce03812752217af116c
kampersanda <shnsk.knd@gmail.com>
2024-09-15 11:49:37 +0900

    add

--- src/trec.rs
+++ src/trec.rs
@@ -32,6 +32,7 @@ use crate::RunBuilder;
 /// assert_eq!(qrels.n_queries(), 2);
 /// assert_eq!(qrels.n_docs(), 5);
 /// assert_eq!(qrels.get_score("q_1", "d_3"), Some(&2));
+/// assert_eq!(qrels.name(), None);
 /// # Ok(())
 /// # }
 /// ```
@@ -63,6 +64,30 @@ where
 ///
 /// Each line should be `<QueryID> <Dummy> <DocID> <Rank> <Score> <RunName>`,
 /// where `<Dummy>` and `<Rank>` are ignored.
+///
+/// # Example
+///
+/// ```rust
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// use emir::trec::parse_run_from_trec;
+///
+/// let data = "
+/// q_1 0 d_1 1 0.5 SAMPLE
+/// q_1 0 d_2 2 0.4 SAMPLE
+/// q_1 0 d_3 3 0.3 SAMPLE
+/// q_2 0 d_3 1 0.3 SAMPLE
+/// q_2 0 d_1 2 0.2 SAMPLE
+/// q_2 0 d_4 3 0.1 SAMPLE
+/// ".trim();
+///
+/// let run = parse_run_from_trec(data.lines())?;
+/// assert_eq!(run.n_queries(), 2);
+/// assert_eq!(run.n_docs(), 6);
+/// assert_eq!(run.get_score("q_1", "d_3"), Some(&0.3.into()));
+/// assert_eq!(run.name(), Some("SAMPLE"));
+/// # Ok(())
+/// # }
+/// ```
 pub fn parse_run_from_trec<I, S>(lines: I) -> Result<Run<String>, EmirError>
 where
     I: Iterator<Item = S>,
