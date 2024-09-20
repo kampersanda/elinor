f603eef4a727364ebdf09d0fc4426f41bb1d54a0
kampersanda <shnsk.knd@gmail.com>
2024-09-20 20:27:06 +0900

    add

--- scripts/compare_with_trec_eval.py
+++ scripts/compare_with_trec_eval.py
@@ -2,30 +2,16 @@
 Script to check the correctness of elinor by comparing its output with trec_eval.
 
 Usage:
-    $ python3 ./scripts/compare_with_trec_eval.py ./target/release/elinor-evaluate
+    $ python3 ./scripts/compare_with_trec_eval.py ./trec_eval-9.0.8/trec_eval ./target/release/elinor-evaluate
 """
 
 import argparse
-import os
 import subprocess
 import sys
 
 
-def download_trec_eval():
-    if os.path.exists("trec_eval-9.0.8"):
-        print("trec_eval-9.0.8 already exists", file=sys.stderr)
-        return
-    subprocess.run("rm -f v9.0.8.tar.gz", shell=True)
-    subprocess.run(
-        "wget https://github.com/usnistgov/trec_eval/archive/refs/tags/v9.0.8.tar.gz",
-        shell=True,
-    )
-    subprocess.run("tar -xf v9.0.8.tar.gz", shell=True)
-    subprocess.run("make -C trec_eval-9.0.8", shell=True)
-
-
-def run_trec_eval() -> dict[str, str]:
-    command = "./trec_eval-9.0.8/trec_eval -c -m all_trec trec_eval-9.0.8/test/qrels.test trec_eval-9.0.8/test/results.test"
+def run_trec_eval(trec_eval: str, qrels_file: str, results_file: str) -> dict[str, str]:
+    command = f"./{trec_eval} -c -m all_trec {qrels_file} {results_file}"
     result = subprocess.run(command, capture_output=True, shell=True)
     parsed: dict[str, str] = {}
     for line in result.stdout.decode("utf-8").split("\n"):
@@ -36,12 +22,12 @@ def run_trec_eval() -> dict[str, str]:
     return parsed
 
 
-def run_elinor(elinor_exe: str) -> dict[str, str]:
+def run_elinor_evaluate(
+    elinor_evaluate: str, qrels_file: str, results_file: str
+) -> dict[str, str]:
     ks = [0, 1, 5, 10, 15, 20, 30, 100, 200, 500, 1000]
-    command = (
-        f"{elinor_exe} -q trec_eval-9.0.8/test/qrels.test -r trec_eval-9.0.8/test/results.test"
-        + "".join([f" -k {k}" for k in ks])
-    )
+    ks_args = " ".join([f"-k {k}" for k in ks])
+    command = f"./{elinor_evaluate} -q {qrels_file} -r {results_file} {ks_args}"
     result = subprocess.run(command, capture_output=True, shell=True)
     parsed: dict[str, str] = {}
     for line in result.stdout.decode("utf-8").split("\n"):
@@ -54,12 +40,18 @@ def run_elinor(elinor_exe: str) -> dict[str, str]:
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("elinor_exe")
+    p.add_argument("trec_eval")
+    p.add_argument("elinor_evaluate")
     args = p.parse_args()
 
-    download_trec_eval()
-    trec_results = run_trec_eval()
-    elinor_results = run_elinor(args.elinor_exe)
+    trec_eval: str = args.trec_eval
+    elinor_evaluate: str = args.elinor_evaluate
+
+    qrels_file = "trec_eval-9.0.8/test/qrels.test"
+    results_file = "trec_eval-9.0.8/test/results.test"
+
+    trec_results = run_trec_eval(trec_eval, qrels_file, results_file)
+    elinor_results = run_elinor_evaluate(elinor_evaluate, qrels_file, results_file)
 
     ks = [5, 10, 15, 20, 30, 100, 200, 500, 1000]
 
