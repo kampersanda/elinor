1a88de51330ab6e549e735ed3393c9720a299841
kampersanda <shnsk.knd@gmail.com>
2024-09-21 11:27:54 +0900

    add

--- src/statistical_tests/paired_student_t_test.rs
+++ src/statistical_tests/paired_student_t_test.rs
@@ -7,6 +7,47 @@ use statrs::statistics::Statistics;
 use crate::errors::ElinorError;
 
 /// Paired Student's t-test.
+///
+/// # Examples
+///
+/// ```
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// use approx::assert_abs_diff_eq;
+/// use elinor::statistical_tests::PairedStudentTTest;
+///
+/// // From Table 5.1 in Sakai's book, "情報アクセス評価方法論".
+/// let a = vec![
+///     0.70, 0.30, 0.20, 0.60, 0.40, 0.40, 0.00, 0.70, 0.10, 0.30, //
+///     0.50, 0.40, 0.00, 0.60, 0.50, 0.30, 0.10, 0.50, 0.20, 0.10,
+/// ];
+/// let b = vec![
+///     0.50, 0.10, 0.00, 0.20, 0.40, 0.30, 0.00, 0.50, 0.30, 0.30, //
+///     0.40, 0.40, 0.10, 0.40, 0.20, 0.10, 0.10, 0.60, 0.30, 0.20,
+/// ];
+///
+/// let paired_samples = a.into_iter().zip(b.into_iter()).map(|(x, y)| (x, y));
+/// let result = PairedStudentTTest::compute(paired_samples)?;
+///
+/// // Various statistics can be obtained.
+/// assert_abs_diff_eq!(result.mean(), 0.0750, epsilon = 1e-4);
+/// assert_abs_diff_eq!(result.var(), 0.0251, epsilon = 1e-4);
+/// assert_abs_diff_eq!(result.effect_size(), 0.473, epsilon = 1e-3);
+/// assert_abs_diff_eq!(result.t_stat(), 2.116, epsilon = 1e-3);
+/// assert_abs_diff_eq!(result.p_value(), 0.048, epsilon = 1e-3);
+///
+/// // Margin of error at a 95% confidence level.
+/// assert_abs_diff_eq!(result.margin_of_error(0.05), 0.0742, epsilon = 1e-4);
+///
+/// // Confidence interval at a 95% confidence level.
+/// let (ci95_btm, ci95_top) = result.confidence_interval(0.05);
+/// assert_abs_diff_eq!(ci95_btm, 0.0750 - 0.0742, epsilon = 1e-4);
+/// assert_abs_diff_eq!(ci95_top, 0.0750 + 0.0742, epsilon = 1e-4);
+///
+/// // Check if the difference is significant at a 95% confidence level.
+/// assert!(result.is_significant(0.05));
+/// # Ok(())
+/// # }
+/// ```
 #[derive(Debug, Clone, Copy)]
 pub struct PairedStudentTTest {
     mean: f64,
@@ -88,19 +129,19 @@ impl PairedStudentTTest {
         self.effect_size
     }
 
-    /// Margin of error at a (1 - significance_level) confidence level.
+    /// Margin of error at a `1 - significance_level` confidence level.
     pub fn margin_of_error(&self, significance_level: f64) -> f64 {
         self.scaled_t_dist
             .inverse_cdf(1.0 - (significance_level / 2.0))
     }
 
-    /// Confidence interval at a (1 - significance_level) confidence level.
+    /// Confidence interval at a `1 - significance_level` confidence level.
     pub fn confidence_interval(&self, significance_level: f64) -> (f64, f64) {
         let moe = self.margin_of_error(significance_level);
         (self.mean - moe, self.mean + moe)
     }
 
-    /// Returns true if the p-value is less than the significance level.
+    /// Returns true if the difference is significant at the given significance level.
     pub fn is_significant(&self, significance_level: f64) -> bool {
         self.p_value < significance_level
     }
@@ -110,34 +151,6 @@ impl PairedStudentTTest {
 mod tests {
     use super::*;
     use approx::assert_abs_diff_eq;
-
-    #[test]
-    fn test_paired_student_t_test_compute() {
-        // From sakai's book
-        let a = vec![
-            0.70, 0.30, 0.20, 0.60, 0.40, 0.40, 0.00, 0.70, 0.10, 0.30, //
-            0.50, 0.40, 0.00, 0.60, 0.50, 0.30, 0.10, 0.50, 0.20, 0.10,
-        ];
-        let b = vec![
-            0.50, 0.10, 0.00, 0.20, 0.40, 0.30, 0.00, 0.50, 0.30, 0.30, //
-            0.40, 0.40, 0.10, 0.40, 0.20, 0.10, 0.10, 0.60, 0.30, 0.20,
-        ];
-        let result =
-            PairedStudentTTest::compute(a.into_iter().zip(b.into_iter()).map(|(x, y)| (x, y)))
-                .unwrap();
-        assert_abs_diff_eq!(result.mean(), 0.0750, epsilon = 1e-4);
-        assert_abs_diff_eq!(result.var(), 0.0251, epsilon = 1e-4);
-        assert_abs_diff_eq!(result.t_stat(), 2.116, epsilon = 1e-3);
-        assert_abs_diff_eq!(result.p_value(), 0.048, epsilon = 1e-3);
-        assert_abs_diff_eq!(result.effect_size(), 0.473, epsilon = 1e-3);
-        assert_abs_diff_eq!(result.margin_of_error(0.05), 0.0742, epsilon = 1e-4);
-
-        let ci_95 = result.confidence_interval(0.05);
-        assert_abs_diff_eq!(ci_95.0, 0.0750 - 0.0742, epsilon = 1e-4);
-        assert_abs_diff_eq!(ci_95.1, 0.0750 + 0.0742, epsilon = 1e-4);
-
-        assert_eq!(result.is_significant(0.05), true);
-    }
 
     #[test]
     fn test_paired_student_t_test_compute_empty() {
