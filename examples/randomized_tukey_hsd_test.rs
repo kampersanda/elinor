6afd32daea449a202be4b9fc66608642641c1181
kampersanda <shnsk.knd@gmail.com>
2024-09-22 22:28:15 +0900

    add

--- examples/randomized_tukey_hsd_test.rs
+++ examples/randomized_tukey_hsd_test.rs
@@ -16,13 +16,20 @@ fn main() -> Result<()> {
         0.40, 0.30, 0.30, 0.20, 0.20, 0.20, 0.10, 0.50, 0.40, 0.30,
     ];
 
-    let tupled_samples = x
-        .into_iter()
-        .zip(y.into_iter())
-        .zip(z.into_iter())
-        .map(|((x, y), z)| [x, y, z]);
-    let result = RandomizedTukeyHsdTest::from_tupled_samples(tupled_samples, 3)?;
+    println!("Comparing two systems.");
+    let tupled_samples = x.iter().zip(y.iter()).map(|(&x, &y)| [x, y]);
+    let result = RandomizedTukeyHsdTest::from_tupled_samples(tupled_samples, 2)?;
+    for (i, j, p_value) in result.p_values() {
+        println!("p-value for {} and {}: {:.4}", i, j, p_value);
+    }
 
+    println!("Comparing three systems.");
+    let tupled_samples = x
+        .iter()
+        .zip(y.iter())
+        .zip(z.iter())
+        .map(|((&x, &y), &z)| [x, y, z]);
+    let result = RandomizedTukeyHsdTest::from_tupled_samples(tupled_samples, 3)?;
     for (i, j, p_value) in result.p_values() {
         println!("p-value for {} and {}: {:.4}", i, j, p_value);
     }
