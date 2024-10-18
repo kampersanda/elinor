6b5291d672cdb155b33e07c4725edfb3adc5dcc3
kampersanda <shnsk.knd@gmail.com>
2024-10-19 03:10:35 +0900

    add

--- src/statistical_tests/randomized_tukey_hsd_test.rs
+++ src/statistical_tests/randomized_tukey_hsd_test.rs
@@ -6,6 +6,7 @@ use rand::SeedableRng;
 use statrs::statistics::Statistics;
 
 use crate::errors::ElinorError;
+use crate::statistical_tests::TwoWayAnovaWithoutReplication;
 
 /// Randomized Tukey HSD test.
 ///
@@ -68,6 +69,7 @@ pub struct RandomizedTukeyHsdTest {
     n_iters: usize,
     random_state: u64,
     p_values: Vec<Vec<f64>>,
+    two_way_anova: TwoWayAnovaWithoutReplication,
 }
 
 impl RandomizedTukeyHsdTest {
@@ -100,11 +102,46 @@ impl RandomizedTukeyHsdTest {
     }
 
     /// p-values for all combinations of systems,
-    /// returning a matrix of size $`n_systems \times n_systems`$.
+    /// returning a matrix of size $`m \times m`$,
+    /// where $`m`$ is the number of systems.
     ///
+    /// The $`(i, j)`$-th element has the p-value
+    /// between the $`i`$-th and $`j`$-th systems.
     /// The diagonal elements are always one.
     pub const fn p_values(&self) -> &Vec<Vec<f64>> {
         &self.p_values
+    }
+
+    /// Effect sizes for all combinations of systems,
+    /// returning a matrix of size $`m \times m`$.
+    /// where $`m`$ is the number of systems.
+    ///
+    /// The $`(i, j)`$-th element is $`\text{ES}_{ij}`$.
+    /// The diagonal elements are always zero.
+    ///
+    /// # Formula
+    ///
+    /// ```math
+    /// \text{ES}_{ij} = \frac{\bar{x}_{i*} - \bar{x}_{j*}}{\sqrt{V_E}}
+    /// ```
+    ///
+    /// where
+    ///
+    /// * $`\bar{x}_{i*}`$ is [the mean score of the $`i`$-th system](TwoWayAnovaWithoutReplication::system_means), and
+    /// * $`V_E`$ is [the residual variance](TwoWayAnovaWithoutReplication::residual_variance).
+    pub fn effect_sizes(&self) -> Vec<Vec<f64>> {
+        let system_means = self.two_way_anova.system_means();
+        let residual_stddev = self.two_way_anova.residual_variance().sqrt();
+        let mut effect_sizes = vec![vec![0.0; self.n_systems]; self.n_systems];
+        for i in 0..self.n_systems {
+            for j in (i + 1)..self.n_systems {
+                let diff = system_means[i] - system_means[j];
+                let effect_size = diff / residual_stddev;
+                effect_sizes[i][j] = effect_size;
+                effect_sizes[j][i] = -effect_size;
+            }
+        }
+        effect_sizes
     }
 }
 
@@ -228,11 +265,15 @@ impl RandomizedTukeyHsdTester {
             }
         }
 
+        let two_way_anova =
+            TwoWayAnovaWithoutReplication::from_tupled_samples(samples.iter(), self.n_systems)?;
+
         Ok(RandomizedTukeyHsdTest {
             n_systems: self.n_systems,
             n_iters: self.n_iters,
             random_state,
             p_values,
+            two_way_anova,
         })
     }
 }
