eceb0d45edc2aaf4e94398f840b86499087b2f20
kampersanda <shnsk.knd@gmail.com>
2024-11-02 23:26:56 +0900

    d

--- elinor-py/elinor/elinor.pyi
+++ elinor-py/elinor/elinor.pyi
@@ -41,9 +41,53 @@ class _StudentTTest:
         """
         pass
 
+    def mean(self) -> float:
+        """Return the mean.
+
+        :return: The mean.
+        """
+        pass
+
+    def variance(self) -> float:
+        """Return the variance.
+
+        :return: The variance.
+        """
+        pass
+
+    def effect_size(self) -> float:
+        """Return the effect size.
+
+        :return: The effect size.
+        """
+        pass
+
+    def t_stat(self) -> float:
+        """Return the t-statistic.
+
+        :return: The t-statistic.
+        """
+        pass
+
     def p_value(self) -> float:
         """Return the p-value.
 
         :return: The p-value.
+        """
+        pass
+
+    def margin_of_error(self, significance_level: float) -> float:
+        """Return the margin of error.
+
+        :param significance_level: The significance level.
+        :return: The margin of error.
+        """
+        pass
+
+    def confidence_interval(self, significance_level: float) -> tuple[float, float]:
+        """Return the confidence interval.
+
+        :param significance_level: The significance level.
+        :return: The confidence interval.
         """
         pass
