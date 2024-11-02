d90d9041a9fd4cb1fc7cf5813583c8b9dc7ab758
kampersanda <shnsk.knd@gmail.com>
2024-11-02 13:40:37 +0900

    add

--- elinor-py/tests/test_elinor.py
+++ elinor-py/tests/test_elinor.py
@@ -1,22 +1,21 @@
-from elinor import elinor
+from elinor import PredRecord, TrueRecord, evaluate
 
 
 def test_evaluate() -> None:
-    true_rels = [
-        {"query_id": "q_1", "doc_id": "d_1", "score": 1},
-        {"query_id": "q_1", "doc_id": "d_2", "score": 0},
-        {"query_id": "q_1", "doc_id": "d_3", "score": 2},
-        {"query_id": "q_2", "doc_id": "d_2", "score": 2},
-        {"query_id": "q_2", "doc_id": "d_4", "score": 1},
+    true_records = [
+        TrueRecord(query_id="q_1", doc_id="d_1", score=1),
+        TrueRecord(query_id="q_1", doc_id="d_2", score=0),
+        TrueRecord(query_id="q_1", doc_id="d_3", score=2),
+        TrueRecord(query_id="q_2", doc_id="d_2", score=2),
+        TrueRecord(query_id="q_2", doc_id="d_4", score=1),
     ]
-    pred_rels = [
-        {"query_id": "q_1", "doc_id": "d_1", "score": 0.5},
-        {"query_id": "q_1", "doc_id": "d_2", "score": 0.4},
-        {"query_id": "q_1", "doc_id": "d_3", "score": 0.3},
-        {"query_id": "q_2", "doc_id": "d_4", "score": 0.1},
-        {"query_id": "q_2", "doc_id": "d_1", "score": 0.2},
-        {"query_id": "q_2", "doc_id": "d_3", "score": 0.3},
+    pred_records = [
+        PredRecord(query_id="q_1", doc_id="d_1", score=0.5),
+        PredRecord(query_id="q_1", doc_id="d_2", score=0.4),
+        PredRecord(query_id="q_1", doc_id="d_3", score=0.3),
+        PredRecord(query_id="q_2", doc_id="d_4", score=0.1),
+        PredRecord(query_id="q_2", doc_id="d_1", score=0.2),
+        PredRecord(query_id="q_2", doc_id="d_3", score=0.3),
     ]
-    scores = elinor._evaluate(true_rels, pred_rels, "ndcg@3")
-    print(scores)
-    assert scores["ndcg@3"] == 0.5
+    result = evaluate(true_records, pred_records, "ndcg@3")
+    assert result.metric == "ndcg@3"
